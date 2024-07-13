8d1d3d05ecfcbf3009a54f39f445cbae120e6b09
zyw <1727448271@qq.com>
2024-07-13 12:44:00 +0800

    完成了登录模块的编写

--- src/main/java/com/maven/recruitment/service/impl/LoginServiceImpl.java
+++ src/main/java/com/maven/recruitment/service/impl/LoginServiceImpl.java
@@ -3,11 +3,13 @@ package com.maven.recruitment.service.impl;
 import com.maven.recruitment.mapper.LoginMapper;
 import com.maven.recruitment.pojo.Info.User;
 import com.maven.recruitment.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maven.recruitment.exception.LoginException;
 
+@Slf4j
 @Service
 public class LoginServiceImpl implements LoginService {
 
@@ -24,7 +26,7 @@ public class LoginServiceImpl implements LoginService {
                 || user.getPhone() == null) {
             throw new LoginException("注册信息不得为空");
         }
-
         loginMapper.signup(user);
+        log.info("用户注册成功");
     }
 }
