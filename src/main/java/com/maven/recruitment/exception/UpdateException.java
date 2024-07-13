d532866d2a33c59ce9289015c679e00a5fbb4a5f
zyw <1727448271@qq.com>
2024-07-13 12:15:12 +0800

    完成了登录模块的编写

---
+++ src/main/java/com/maven/recruitment/exception/UpdateException.java
@@ -0,0 +1,11 @@
+package com.maven.recruitment.exception;
+
+public class UpdateException extends BaseException{
+    public UpdateException() {
+
+    }
+
+    public UpdateException(String msg) {
+        super(msg);
+    }
+}
